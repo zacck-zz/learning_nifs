aa1d7802999f04cf1e2691391710ac6a437d4ea4
Zacck Osiemo <coderv63@gmail.com>
2019-12-18 17:48:45 +0200

    Initialize mix project

---
+++ lib/learning_nifs.ex
@@ -0,0 +1,18 @@
+defmodule LearningNifs do
+  @moduledoc """
+  Documentation for LearningNifs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LearningNifs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/learning_nifs_test.exs
@@ -0,0 +1,8 @@
+defmodule LearningNifsTest do
+  use ExUnit.Case
+  doctest LearningNifs
+
+  test "greets the world" do
+    assert LearningNifs.hello() == :world
+  end
+end
